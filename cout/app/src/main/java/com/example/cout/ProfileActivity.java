01a87c30dbe9a89d8181df6e3dbe6966fad48e98
mehakkkk <mehakgupta160@gmail.com>
2020-11-28 13:54:05 +0530

    progressbar added, leaderboard bugs resolved,data added into profile activity

--- cout/app/src/main/java/com/example/cout/ProfileActivity.java
+++ cout/app/src/main/java/com/example/cout/ProfileActivity.java
@@ -1,5 +1,7 @@
 package com.example.cout;
 
+import android.util.Log;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,16 +12,27 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.*;
+import org.w3c.dom.Text;
+
+import java.util.Map;
 
 public class ProfileActivity extends AppCompatActivity {
     private Button Logout;
     private FirebaseAuth firebaseAuth;
+    private TextView nameLabel;
+    private TextView scoreLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        nameLabel = (TextView)findViewById(R.id.name);
+        scoreLabel = (TextView)findViewById(R.id.score);
 
         setupUIViews();
 
@@ -35,7 +48,28 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     private void setupUIViews(){                                // setting the values from ui to these variables
+
         Logout = (Button)findViewById(R.id.btnLogout);
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        System.out.println("email:  "+currentUser.getEmail()+" UID: "+currentUser.getUid());
+        final FirebaseFirestore db= FirebaseFirestore.getInstance();
+        nameLabel.setText("Fetching data");
+        db.collection("UsersInfo").document(currentUser.getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        nameLabel.setText("Name: "+document.get("name"));
+                        scoreLabel.setText("Score: "+document.get("Score"));
+                    }
+                }
+            }
+        });
+
+
+
     }
 
     private void Logoutfn(){   //defining logout fn
--- cout/app/src/main/java/com/example/cout/activity_leaderboard.java
+++ cout/app/src/main/java/com/example/cout/activity_leaderboard.java
@@ -1,27 +1,70 @@
 package com.example.cout;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.*;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.auth.User;
 
-import java.util.TreeMap;
-import java.util.Vector;
+
+import java.util.*;
+
+
 
 public class activity_leaderboard extends AppCompatActivity {
 
-    final Vector userScore = new Vector();
+
+
+    final ArrayList<String> list = new ArrayList<String>();
+    ListView userScoreList;
+    ProgressBar progressBar;
+    TextView progressBarInsideText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
         getScore();
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        //progressBarInsideText = (TextView)findViewById(R.id.progressBarInsideText) ;
+        userScoreList = (ListView) findViewById(R.id.userScoreList);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,  android.R.layout.simple_expandable_list_item_1, list);
+
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                progressBar.setVisibility(View.GONE);
+                //progressBarInsideText.setVisibility(View.GONE);
+            }
+
+        }, 3000);
+
+       // userScoreList.setAdapter(arrayAdapter);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                userScoreList.setAdapter(arrayAdapter);
+                ///add set Adapter here
+
+            }
+        },1000);
+
 
     }
 
@@ -29,74 +72,31 @@ public class activity_leaderboard extends AppCompatActivity {
     {
         final FirebaseFirestore db= FirebaseFirestore.getInstance();
 
-        /**to get a single document
-        String currentuser = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        CollectionReference colRef= db.collection("UsersInfo");
-        colRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if(task.isSuccessful())
-                {
-                    CoSnapshot documentSnapshot = task.getResult();
-                    if(documentSnapshot.exists())
-                    {
-                        Log.d("data",documentSnapshot.getData().toString());
-                        System.out.println("Helloooo");
-                        System.out.println(documentSnapshot.getData());
-                    }
-                    else
-                    {
-                        Log.d("TAG","No such document");
-                    }
-                }
-            }
-        });**/
 
 
-
-//        db.collection("UsersInfo")
-//                .get()
-//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//
-//                        if (task.isSuccessful() ) {
-//                            System.out.println(task.isSuccessful()+" task check");
-//                            for (int i=0;i<2;i++) {
-//                                QueryDocumentSnapshot document= (QueryDocumentSnapshot)task.getResult();
-//
-//                                System.out.println("Helloooo");
-//                                System.out.println(document.getData());
-//                                userScore.add(document.getData());
-//                                Log.d("TAG", document.getId() + " => " + document.getData());
-//
-//                            }
-//                        }
-//                    }
-//                });
-
-        db.collection("UsersInfo").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        db.collection("UsersInfo").orderBy("Score", Query.Direction.DESCENDING).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
-                    //List list = new ArrayList<>();
-                    TreeMap list = new TreeMap();
 
+
+
+                    //HashMap<String,Integer>list = new HashMap<String, Integer>();
                     for (QueryDocumentSnapshot document : task.getResult()) {
-                        list.put(document.get("Score"), document.getData());
+                        Integer score = Integer.parseInt(document.get("Score").toString());
+                        String name = document.get("name").toString();
+                        UserInfo user = new UserInfo(name,score);
+                        list.add("Name: "+user.getName()+"\nScore: "+user.getScore());
+
                     }
-                    //list.descendingMap();
-                    Log.d("TAG", list.descendingMap().toString());
-                    TextView textView = (TextView) findViewById(R.id.leaderboardview);
-                    textView.setText(list.descendingMap().toString());
+                    System.out.println("\n"+list);
+
 
                 } else {
                     Log.d("TAG", "Error getting document");
                 }
             }
             });
-
-        //iterating through vector
 
 
 
